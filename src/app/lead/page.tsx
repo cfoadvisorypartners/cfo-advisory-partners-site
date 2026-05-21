4d7086eef058d5dc9c52979872d3cfd8e15a24eb
John Moss <info@cfoadvisorypartners.net>
2026-05-21 09:29:51 -0600

    Add page-level Meta Pixel

--- src/app/lead/page.tsx
+++ src/app/lead/page.tsx
@@ -29,6 +29,32 @@ export default function LeadPage() {
 
   return (
     <div className="min-h-screen bg-stone-50 text-slate-900">
+      <script
+        id="meta-pixel-1565846307843505-lead"
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1565846307843505');
+fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1565846307843505&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
       <header className="border-b border-slate-200 bg-white">
         <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3 lg:px-10">
           <Link href="/" className="block">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,32 @@ export default function HomePage() {
 
   return (
     <main className="bg-white text-slate-900">
+      <script
+        id="meta-pixel-1565846307843505-home"
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1565846307843505');
+fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1565846307843505&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
       <section className="relative isolate min-h-screen overflow-hidden">
         <div className="absolute inset-0">
           <video
